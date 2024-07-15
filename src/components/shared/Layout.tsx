3424ecf287f0d18d6a45b284d716cac1f074d6be
iamparves <parves.pe.wb.h@gmail.com>
2024-07-16 00:02:02 +0600

    whatsapp contact added

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import ScrollToTop from "./ScrollToTop";
+import WhatsappButton from "./WhatsappButton";
 
 const Layout = () => {
   return (
     <div className="min-h-svh bg-[#f5f5f5] font-poppins">
       <Navbar />
+      <WhatsappButton />
       <Outlet />
       <Footer />
       <ScrollToTop />
---
+++ src/components/shared/WhatsappButton.tsx
@@ -0,0 +1,21 @@
+import { FaWhatsapp } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+const WhatsappButton = () => {
+  return (
+    <Link
+      to="https://wa.me/447946193824"
+      className="fixed right-0 top-[80px] z-[99] grid translate-x-[calc(100%-40px)] grid-cols-[auto_1fr] items-center bg-[#00D757] text-white duration-300 hover:translate-x-0 lg:top-[130px]"
+      target="_blank"
+    >
+      <span className="flex w-10 justify-center py-2">
+        <FaWhatsapp className="text-2xl" />
+      </span>
+      <span className="block pr-2 text-[15px] font-medium">
+        +44-7946-193824
+      </span>
+    </Link>
+  );
+};
+
+export default WhatsappButton;
